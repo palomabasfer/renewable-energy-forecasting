cc235bd5e45fbf925ffcafb317bef13653371f77
Paloma Bas Fernández <palomabasfer@gmail.com>
2026-08-07 12:12:35 +0200

    test(viz): add unit tests for fan chart formatter

---
+++ tests/test_fan_chart.py
@@ -0,0 +1,6 @@
+import numpy as np
+from src.evaluation.fan_chart import generate_fan_chart_series
+
+def test_fan_chart():
+    res = generate_fan_chart_series(np.array([1]), np.array([2]), np.array([3]))
+    assert res['p10'] == [1]
